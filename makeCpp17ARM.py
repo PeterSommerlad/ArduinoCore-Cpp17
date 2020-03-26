f279e7a76f54b177bafec8b710862a49a3e6302a
Peter Sommerlad <peter.sommerlad@hsr.ch>
2020-03-26 19:48:12 +0100

    safe almost all from big file hickupt

---
+++ makeCpp17ARM.py
@@ -0,0 +1,39 @@
+import hashlib
+import json
+import os.path
+import sys
+import tarfile
+
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, 'w:gz') as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+    hasher = hashlib.sha256()
+    with open(output_filename, 'rb') as fh:
+        data = fh.read()
+        hasher.update(data)
+        return hasher.hexdigest(), len(data)
+
+
+version = sys.argv[1]
+tar_name = 'Cpp17ARM-package-'+version+'.tar.gz'
+
+print( tar_name , version )
+
+digest, size = make_tarfile(tar_name, 'Cpp17ARM-package/.')
+
+print( digest, "\nsize = ", str(size)) 
+
+with open('package_Cpp17ARM_index.json', 'r+') as index_file:
+    data=json.load(index_file)
+    for p in data['packages'][0]['platforms']:
+        if p['version'] == version:
+            p['archiveFileName']=tar_name
+            p['checksum']='SHA-256:'+digest
+            p['size']=str(size)
+            p['url']='file://localhost/Users/sop/Arduino/ArduinoCore-Cpp17/'+tar_name
+            break
+    index_file.seek(0)
+    json.dump(data, index_file, indent=4, sort_keys=True)
+    index_file.truncate()
